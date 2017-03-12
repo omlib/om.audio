0307e4c1f0f3fad4dd26549864e3d327f48002d2
tong <tong@disktree.net>
2017-03-12 16:04:10 +0100

    AudioBufferLoader: use promise

--- src/om/audio/AudioBufferLoader.hx
+++ src/om/audio/AudioBufferLoader.hx
@@ -1,48 +1,26 @@
 package om.audio;
 
-import js.Error;
+import js.Browser.window;
+import js.Promise;
 import js.html.ArrayBuffer;
-import js.html.XMLHttpRequest;
 import js.html.audio.AudioContext;
 import js.html.audio.AudioBuffer;
 
 class AudioBufferLoader {
 
-	public static function load( context : AudioContext, url : String, callback : Error->AudioBuffer->Void ) {
-		loadArrayBuffer( context, url, function(e,data){
-			if( e != null ) callback( e, null ) else {
-				context.decodeAudioData( data, function(buf) callback( null, buf ) );
-			}
+	public static inline function decodeAudioData( context : AudioContext, buf : ArrayBuffer ) : Promise<AudioBuffer> {
+		return context.decodeAudioData( buf );
+	}
+
+	public static function loadArrayBuffer( url : String ) : Promise<ArrayBuffer> {
+		return window.fetch( url ).then( function(res) {
+			return res.arrayBuffer();
 		});
 	}
 
-	public static function loadArrayBuffer( context : AudioContext, url : String, callback : Error->ArrayBuffer->Void ) {
-		var errorReported = false;
-		var xhr = new XMLHttpRequest();
-		xhr.open( "GET", url, true );
-		xhr.responseType = ARRAYBUFFER;
-		/*
-		xhr.onerror = function(e){
-			trace(e);
-			callback( e, null );
-			return;
-		}
-		*/
-		xhr.onreadystatechange = function(e){
-			//#if (atom||electron)
-			// xhr.status == always 0
-			//#else
-			if( xhr.status != 200 && !errorReported ) {
-				errorReported = true;
-				callback( new om.error.FileNotFound( url ), null );
-				return;
-			}
-			//#end
-		}
-		xhr.onload = function(e){
-			callback( null, xhr.response );
-		}
-		xhr.send();
+	public static function loadAudioBuffer( context : AudioContext, url : String ) : Promise<AudioBuffer> {
+		return loadArrayBuffer( url ).then( function(buf:ArrayBuffer) {
+			return context.decodeAudioData( buf );
+		});
 	}
-
 }
