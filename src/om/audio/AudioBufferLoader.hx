7a72e950a3d5ca567462cac8b20e3b65c56ad7e3
tong <tong@disktree.net>
2017-03-26 11:54:51 +0200

    AudioBufferLoader: use local fs (not http://) on nodejs

--- src/om/audio/AudioBufferLoader.hx
+++ src/om/audio/AudioBufferLoader.hx
@@ -8,19 +8,42 @@ import js.html.audio.AudioBuffer;
 
 class AudioBufferLoader {
 
-	public static inline function decodeAudioData( context : AudioContext, buf : ArrayBuffer ) : Promise<AudioBuffer> {
-		return context.decodeAudioData( buf );
+	public static inline function decodeAudioData( ctx : AudioContext, buf : ArrayBuffer ) : Promise<AudioBuffer> {
+		return ctx.decodeAudioData( buf );
 	}
 
 	public static function loadArrayBuffer( url : String ) : Promise<ArrayBuffer> {
+
+		#if electron
+		return new Promise( function(resolve,reject){
+			js.node.Fs.readFile( url, function(e,buf){
+				if( e != null )
+					reject(e)
+				else
+					resolve( om.util.ArrayBufferUtil.buf2ab( buf ) );
+			} );
+		});
+
+		#else
 		return window.fetch( url ).then( function(res) {
 			return res.arrayBuffer();
 		});
+
+		#end
 	}
 
-	public static function loadAudioBuffer( context : AudioContext, url : String ) : Promise<AudioBuffer> {
+	public static function loadAudioBuffer( ctx : AudioContext, url : String ) : Promise<AudioBuffer> {
 		return loadArrayBuffer( url ).then( function(buf:ArrayBuffer) {
-			return context.decodeAudioData( buf );
+			return ctx.decodeAudioData( buf );
 		});
+	}
+
+	static function toArrayBuffer( buf : js.node.Buffer ) {
+	    var ab = new ArrayBuffer(buf.length);
+	    var view = new js.html.Uint8Array(ab);
+		for( i in 0...buf.length ) {
+	        view[i] = buf[i];
+	    }
+	    return ab;
 	}
 }
